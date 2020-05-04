68a36911b6b292397c9a559e34132daba9401e6d
Viktor <victor318x@gmail.com>
2020-05-04 15:06:11 +0800

    emzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+
+// 单元测试适合用shallow,集成测试适合用mount
+test('renders', () => {
+   const wrapper = Enzyme.shallow(<App/>)
+   expect(wrapper.find('.App').length).toBe(1)
+   console.log(wrapper.debug())
+   expect(wrapper.find('.App').prop('title')).toBe('hello')
+});
+
+test('renders with data-name', () => {
+  const wrapper = Enzyme.shallow(<App/>)
+  console.log(wrapper.find('[data-name="App"]'))
+  expect(wrapper.find('[data-name="App"]')).toExist()
+  expect(wrapper.find('[data-name="App"]')).toHaveProp('title','hello')
+  expect(wrapper).toMatchSnapshot();
 });
